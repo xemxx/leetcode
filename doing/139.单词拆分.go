b75a7f20c0e7f5af2b8467f04d9237e93b894572
xem <1007441364@qq.com>
2019-04-19 17:48:28 +0800

    04-19

--- doing/139.单词拆分.go
+++ doing/139.单词拆分.go
@@ -8,14 +8,16 @@ func main() {
 	//很简单的想到深搜，结果一下案例超时，我自己的mac本地跑了500多秒没跑完。。。下面用动规试试
 	s := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
 	wordDict := []string{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"}
-	fmt.Println(wordBreak(s, wordDict))
+	fmt.Println(wordBreak1(s, wordDict))
 
 }
 func wordBreak(s string, wordDict []string) bool {
-
-	return dfs(s, wordDict, 0)
+	//return dfs2(s, wordDict, 0)
+	memo := make([]bool, len(s)+1)
+	return dfs2_1(s, wordDict, 0, memo)
 }
 
+//第一种递归，遍历字典
 func dfs(s string, wordDict []string, index int) bool {
 
 	if index == len(s) {
@@ -36,7 +38,68 @@ func dfs(s string, wordDict []string, index int) bool {
 	return false
 }
 
+//第二种递归，遍历字符串
+func dfs2(s string, wordDict []string, index int) bool {
 
+	if index == len(s) {
+		return true
+	}
+	for i := index + 1; i <= len(s); i++ {
+		fmt.Println(i)
+		if inword(s[index:i], wordDict) && dfs2(s, wordDict, i) {
+			//只要找到一个成功的就直接成功
+			return true
+		}
+	}
+	return false
+}
+
+func inword(s string, wordDict []string) bool {
+	for _, v := range wordDict {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+
+//第二种优化 成功通过
+func dfs2_1(s string, wordDict []string, index int, memo []bool) bool {
+
+	if index == len(s) {
+		return true
+	}
+	if memo[index] == true {
+		return false
+	}
+	for i := index + 1; i <= len(s); i++ {
+		if memo[i] == true {
+			continue
+		}
+		//此处判断是否在字典内可以优化为map[string]bool 直接判断
+		if inword(s[index:i], wordDict) && dfs2_1(s, wordDict, i, memo) {
+			//只要找到一个成功的就直接成功
+			return true
+		}
+	}
+	memo[index] = true
+	return false
+}
+
+//动态规划 能过leetcode f[i]表示i长度的字符串能否被字典拼接
+func wordBreak1(s string, wordDict []string) bool {
+	f := make([]bool, len(s)+1)
+	f[0] = true
+	for i := 1; i <= len(s); i++ {
+		for _, v := range wordDict {
+			if i >= len(v) && f[i-len(v)] && v == s[i-len(v):i] {
+				f[i] = true
+				break
+			}
+		}
+	}
+	return f[len(s)]
+}
 
 /*
  * @lc app=leetcode.cn id=139 lang=golang
