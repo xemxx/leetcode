d8545576476a0328926fdd40e77a29b734f9b2df
xem <1007441364@qq.com>
2019-04-23 15:10:48 +0800

    04-23

--- doing/208.实现-trie-前缀树.go
+++ doing/208.实现-trie-前缀树.go
@@ -1,46 +1,144 @@
 package main
 
-type Trie struct {
-	count int
-	child map[string]Trie
-}
+import "fmt"
 
 func main() {
-	word := "dasd"
-	prefix := "da"
+	word := "apple"
+	prefix := "apple"
 	obj := Constructor()
 	obj.Insert(word)
-	param_2 := obj.Search(word)
-	param_3 := obj.StartsWith(prefix)
+	fmt.Println(obj.Search(prefix))
+	fmt.Println(obj.StartsWith(prefix))
 }
 
-/** Initialize your data structure here. */
+// type Trie struct {
+// 	count int
+// 	child map[string]*Trie
+// }
+// func Constructor() Trie {
+// 	return Trie{0, map[string]*Trie{}}
+// }
+
+// func (t *Trie) Insert(word string) {
+// 	node := t
+// 	for i := 0; i < len(word); i++ {
+// 		v := word[i : i+1]
+// 		_, ok := node.child[v]
+// 		if !ok {
+// 			node.count++
+// 			newNode := Trie{0, map[string]*Trie{}}
+// 			node.child[v] = &newNode
+// 		}
+// 		node = node.child[v]
+// 	}
+// }
+
+// func (t *Trie) Search(word string) bool {
+// 	node := t
+// 	previous := false
+// 	for i := 0; i < len(word); i++ {
+// 		v := word[i : i+1]
+// 		_, ok := node.child[string(v)]
+// 		if ok {
+// 			//fmt.Println(3, v)
+// 			previous = true
+// 			node = node.child[string(v)]
+// 		} else if previous == true {
+// 			//fmt.Println(2)
+// 			return false
+// 		} else {
+// 			//fmt.Println(1)
+// 			for _, v := range node.child {
+// 				if v.Search(word) {
+// 					return true
+// 				}
+// 			}
+// 			//此处应终止外层循环，防止产生断层匹配
+// 			return false
+// 		}
+// 	}
+// 	return previous
+// }
+
+// func (t *Trie) StartsWith(prefix string) bool {
+// 	node := t
+// 	for _, v := range prefix {
+// 		_, ok := node.child[string(v)]
+// 		if ok {
+// 			node = node.child[string(v)]
+// 		} else {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+//由于上述代码理解错误tire的意思，作废，我也不知道我写了个什么数据结构出来
+
+type Trie struct {
+	count int //用于统计词频
+	isKey bool
+	child map[string]*Trie //此处可用[26]*Tire继续优化，但考虑到实际业务，用map更为稳妥
+}
+
 func Constructor() Trie {
-	return Trie{0, map[string]Trie{}}
+	return Trie{0, false, map[string]*Trie{}}
 }
 
-/** Inserts a word into the trie. */
 func (t *Trie) Insert(word string) {
-	tmp:=
+	node := t
 	for i := 0; i < len(word); i++ {
 		v := word[i : i+1]
-		_, ok := t.child[v]
-		if ok {
-			continue
+		_, ok := node.child[v]
+		if !ok {
+			newNode := Trie{0, false, map[string]*Trie{}}
+			node.child[v] = &newNode
 		}
-		t.count += 1
-		t.child
+		node = node.child[v]
 	}
+	node.count++
+	node.isKey = true
 }
 
-/** Returns if the word is in the trie. */
 func (t *Trie) Search(word string) bool {
-
+	node := t
+	for _, v := range word {
+		node = node.child[string(v)]
+		if node == nil {
+			return false
+		}
+	}
+	return node.isKey
+	// 对性能的强迫症使我用了上述代码
+	// for i := 0; i < len(word) && node != nil; i++ {
+	// 	v := word[i : i+1]
+	// 	node = node.child[v]
+	// }
+	// if node != nil && node.isKey {
+	// 	return true
+	// }
+	// return false
 }
 
-/** Returns if there is any word in the trie that starts with the given prefix. */
 func (t *Trie) StartsWith(prefix string) bool {
-
+	node := t
+	for _, v := range prefix {
+		node = node.child[string(v)]
+		if node == nil {
+			return false
+		}
+	}
+	return true
+	// 同上
+	// for _, v := range prefix {
+	// 	_, ok := node.child[string(v)]
+	// 	if ok {
+	// 		node = node.child[string(v)]
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
+	// return true
 }
 
 /**
