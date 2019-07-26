aad43bd7e68abd4e46bb1f161aa10e1f664f42aa
xem <1007441364@qq.com>
2019-07-26 20:04:23 +0800

    doing 406

--- test.go
+++ test.go
@@ -8,12 +8,12 @@ func main() {
 }
 func dp(s string, n int, re string) {
 	ls := len(s)
-	if ls<=n*3 && ls>n{
+	if ls <= n*3 && ls > n {
 		if n == 1 {
 			fmt.Println(re + s)
 		}
-		for i:=1;i<=ls && i<=3;i++{
-			dp(s[i:],n-1,re+s[:i]+"*") 
+		for i := 1; i <= ls && i <= 3; i++ {
+			dp(s[i:], n-1, re+s[:i]+"*")
 		}
 	}
 }
