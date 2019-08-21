7aace0fa7e97cb38544a2d09f10a4d2063b67a83
xem <1007441364@qq.com>
2019-08-21 19:20:02 +0800

    update readme

--- 面试题/360/求组合体的表面积/main.go
+++ 面试题/360/求组合体的表面积/main.go
@@ -1,12 +1,18 @@
 package main
 
+/*
+题目： 用一个二维数组表示一个由多个体积为1的正方体组成的组合体（多面体），每个元素的数值n表明此位置存在n个正方体堆叠，请求出其表面积，
+输入：2 2
+	 2 1
+	 1 1
+输出：20
+*/
 import (
 	"fmt"
 )
 
 func main() {
 	fmt.Println(solution([][]int{[]int{2, 2}, []int{2, 1}, []int{1, 1}}))
-
 }
 
 func solution(arr [][]int) int {
