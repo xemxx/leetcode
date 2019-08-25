5e029a469cb739598babba7df0e08b23724647f0
xem <1007441364@qq.com>
2019-08-25 23:11:38 +0800

    字节跳动正是批 解法思路完善

--- 面试题/字节跳动正式批/2048/solution.go
+++ 面试题/字节跳动正式批/2048/solution.go
@@ -8,13 +8,19 @@ import (
 func main() {
 	var n int
 	fmt.Scanln(&n)
-	m := [][]int{}
-	for i := 0; i <= 3; i++ {
-		arr := make([]int, 4)
-		for j := 0; j <= 3; j++ {
-			fmt.Scan(&arr[j])
-		}
-		m = append(m, arr)
+	// m := [][]int{}
+	// for i := 0; i <= 3; i++ {
+	// 	arr := make([]int, 4)
+	// 	for j := 0; j <= 3; j++ {
+	// 		fmt.Scan(&arr[j])
+	// 	}
+	// 	m = append(m, arr)
+	// }
+	m := [][]int{
+		{0, 0, 0, 2},
+		{2, 1, 1, 2},
+		{2, 0, 4, 8},
+		{8, 0, 4, 8},
 	}
 	q, w := 1, 1
 	e, r := 0, 0
@@ -29,37 +35,65 @@ func main() {
 		q, w, e, r = 1, -1, 0, 3
 	}
 	for i := e; 0 <= i && i <= 3; i += q {
-		j := r
-		for j = r; 0 <= j && j <= 3; j += w {
+		x, j := r, r
+		tail := 4
+		pid := r + 3*w
+
+		for j += w; tail > 0; tail-- {
 			if n == 3 || n == 4 {
-				if m[i][j] == 0 {
-					for y := j; 0 < y && y < 3; y += w {
+				if m[i][x] == 0 {
+					y := 0
+					for y = x; y != pid; y += w {
 						m[i][y] = m[i][y+w]
 					}
-					m[i][3] = 0
-				} else if m[i][j] == m[i][j+1] {
-					m[i][j+1] = m[i][j] + m[i][j+1]
-					for y := j; 0 < y && y < 3; y += w {
+					m[i][y] = 0
+				} else if m[i][j] == 0 {
+					y := 0
+					for y = j; y != pid; y += w {
 						m[i][y] = m[i][y+w]
 					}
-					m[i][3] = 0
+					m[i][y] = 0
+				} else if m[i][j] == m[i][x] {
+					m[i][x] = m[i][j] + m[i][x]
+					y := 0
+					for y = j; 0 < y && y < 3; y += w {
+						m[i][y] = m[i][y+w]
+					}
+					m[i][y] = 0
+					x = j
+					j += w
+				} else {
+					x = j
+					j += w
 				}
 			} else {
-				if m[j][j] == 0 {
-					for y := j; 0 < y && y < 3; y += w {
+				if m[x][i] == 0 {
+					y := 0
+					for y = x; y != pid; y += w {
 						m[y][i] = m[y+w][i]
 					}
-					m[3][i] = 0
-				} else if m[j][i] == m[j+1][i] && j+1 != j {
-					m[j+1][i] = m[j][i] + m[j+1][i]
-					for y := j; 0 < y && y < 3; y += w {
+					m[y][i] = 0
+				} else if m[j][i] == 0 {
+					y := 0
+					for y = j; y != pid; y += w {
 						m[y][i] = m[y+w][i]
 					}
-					m[3][i] = 0
+					m[y][i] = 0
+				} else if m[j][i] == m[x][i] {
+					m[x][i] = m[j][i] + m[x][i]
+					y := 0
+					for y = j; y != pid; y += w {
+						m[y][i] = m[y+w][i]
+					}
+					m[y][i] = 0
+					x = j
+					j += w
+				} else {
+					x = j
+					j += w
 				}
 			}
 		}
-		j = r
 	}
 	for i := range m {
 		s := ""
--- 面试题/字节跳动正式批/豆油瓶/solution.go
+++ 面试题/字节跳动正式批/豆油瓶/solution.go
@@ -8,5 +8,5 @@ func main() {
 	if err != nil {
 		return
 	}
-	
+
 }
