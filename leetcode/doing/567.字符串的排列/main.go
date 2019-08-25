e923c42978a15a15baa96c7202b531c01366d7fe
xem <1007441364@qq.com>
2019-08-25 23:12:22 +0800

    字节跳动正是批 解法思路完善

--- leetcode/doing/567.字符串的排列/main.go
+++ leetcode/doing/567.字符串的排列/main.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import "fmt"
@@ -6,12 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println(checkInclusion("an", "asdbass"))
 }
-=======
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println(checkInclusion("an", "asdbass"))
-}
->>>>>>> a40972a733e51c0bb197d7b6ec9daab7c7f31bc1
--- leetcode/doing/567.字符串的排列/solution.go
+++ leetcode/doing/567.字符串的排列/solution.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 /*
@@ -10,16 +9,3 @@ func checkInclusion(s1 string, s2 string) bool {
 
 	return false
 }
-=======
-package main
-
-/*
- * @lc app=leetcode.cn id=567 lang=golang
- *
- * [567] 字符串的排列
- */
-func checkInclusion(s1 string, s2 string) bool {
-
-	return false
-}
->>>>>>> a40972a733e51c0bb197d7b6ec9daab7c7f31bc1
