a40972a733e51c0bb197d7b6ec9daab7c7f31bc1
xem <1007441364@qq.com>
2019-08-25 18:33:29 +0800

    快手 笔试

---
+++ 面试题/快手/合并内容流/solution.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	bytes, _, err := reader.ReadLine()
+	str := string(bytes)
+	now := []int{}
+	start := 0
+	for i := range str {
+		if str[i] == ' ' {
+			num, _ := strconv.Atoi(str[start:i])
+			now = append(now, num)
+			start = i + 1
+		}
+	}
+	num, _ := strconv.Atoi(str[start:])
+	now = append(now, num)
+
+	reader = bufio.NewReader(os.Stdin)
+	bytes, _, err = reader.ReadLine()
+	str = string(bytes)
+	char := []string{}
+	start = 0
+	for i := range str {
+		if str[i] == ' ' {
+			char = append(char, str[start:i])
+			start = i + 1
+		}
+	}
+	char = append(char, str[start:])
+
+	for i := range num {
+		if i%4 == 0 {
+			fmt.Print("%v ", char[i])
+		} else {
+			fmt.Print("%v ", num[i])
+		}
+	}
+
+}
